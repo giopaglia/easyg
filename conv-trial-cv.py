9f36d7683bc4d57483418de35f8cb6dc55f8c17d
Shruthi Dinakaran <35603044+ShruthiDinakaran@users.noreply.github.com>
2019-03-04 18:52:52 +0100

    Add files via upload

---
+++ conv-trial-cv.py
@@ -0,0 +1,81 @@
+import sys
+import pandas as pd
+import numpy as np
+import sklearn
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv1D, Flatten
+from keras.utils import to_categorical
+from sklearn.model_selection import StratifiedKFold
+
+############################################################
+# Convolutional Network - TEST:
+#  - Read training, validation and test data
+#  - Train a convolutional network with one hidden layers
+#  - Test data
+############################################################
+
+seed =7
+DATA_TRAIN = 'data/shifted-cv.csv'
+#DATA_VALID = 'data/validation.csv'
+#DATA_TEST  = 'data/test.csv'
+
+
+# Retrieve data from files
+data_train = pd.read_csv(DATA_TRAIN, sep=',', header=None)
+#data_valid = pd.read_csv(DATA_VALID, sep=',', header=None)
+#data_test  = pd.read_csv(DATA_TEST,  sep=',', header=None)
+
+# Separate datapoints (x) and label (y) from each row
+def unpack_data(data):
+    return (data.values[:,1:], data.values[:,0])
+
+(x_train, y_train) = unpack_data(data_train)
+#(x_valid, y_valid) = unpack_data(data_valid)
+#(x_test,  y_test)  = unpack_data(data_test)
+
+print()
+print(" TRAINING  SAMPLES: ", len(x_train))
+#print("VALIDATION SAMPLES: ", len(x_valid))
+#print("   TEST    SAMPLES: ", len(x_test))
+
+x_train = np.array(x_train).reshape(len(x_train),140,1).astype('float32')
+#x_valid = np.array(x_valid).reshape(len(x_valid),140,1).astype('float32')
+#x_test  = np.array(x_test ).reshape(len(x_test ),140,1).astype('float32')
+
+#y_valid_raw  = y_valid
+y_train = np.array((y_train)).reshape(len(y_train),1).astype('int32') # y_train = np.array(to_categorical(y_train)).reshape(len(y_train),2).astype('int32')
+#y_valid = np.array((y_valid)).reshape(len(y_valid),1).astype('int32') # y_valid = np.array(to_categorical(y_valid)).reshape(len(y_valid),2).astype('int32')
+#y_test  = np.array((y_test )).reshape(len(y_test ),1).astype('int32') # y_test  = np.array(to_categorical(y_test )).reshape(len(y_test ),2).astype('int32')
+
+print()
+print (" TRAINING  DATA SHAPE:\t", x_train.shape, "\t-> ", y_train.shape)
+#print ("VALIDATION DATA SHAPE:\t", x_valid.shape, "\t-> ", y_valid.shape)
+#print ("   TEST    DATA SHAPE:\t", x_test.shape, "\t-> ", y_test.shape)
+print()
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+cvscores = []
+for train, test in kfold.split(x_train, y_train):
+
+# Create model
+  model=Sequential()
+
+# Add model layers
+  model.add(Conv1D(15, kernel_size=3, activation='relu', input_shape=(140,1)))
+  model.add(Flatten())
+  model.add(Dense(1, activation='sigmoid'))
+
+# Compile model using accuracy to measure model performance
+  model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+  model.fit(x_train, y_train, epochs=30)
+# evaluate the model
+scores = model.evaluate(x_train[test], y_train[test], verbose =0)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+cvscores.append(scores[1] * 100)
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+
+# Test on the validation set
+#_, acc = model.evaluate(x_valid, y_valid)
+#print('Accuracy: {0}'.format(acc * 100))
---
+++ rnn_cv.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import sklearn
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers.recurrent import LSTM
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import StratifiedKFold
+
+seed = 7
+
+DATA ='data/shifted-cv.csv'
+
+
+#DATA_TRAIN = 'data/train.csv'
+#DATA_VALID = 'data/validation.csv'
+
+def load_dataset(path):
+    dataset = pd.read_csv(path, sep=',', header=None)
+    X = dataset.values[:, 1:]
+    Y = dataset.values[:, 0].astype('int32')
+    X =  X.reshape(X.shape[0], 1, X.shape[1])
+    return (X, Y)
+	
+(input,output) = load_dataset(DATA) 
+	
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+cvscores = []
+for train, test in kfold.split(input, output):
+
+#def build_model():
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(None, 140)))
+    model.add(Dense(1, activation='sigmoid'))
+
+    optimizer = Adam(lr=0.001)
+
+    model.compile(
+            loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+  #  return model
+
+
+#(train_x, train_y) = load_dataset(DATA_TRAIN)
+#(valid_x, valid_y) = load_dataset(DATA_VALID)
+
+#model = build_model()
+
+es = EarlyStopping(
+        monitor='val_acc',
+        min_delta=0,
+        patience=2,
+        verbose=0,
+        mode='auto',
+        restore_best_weights=True)
+
+model.fit(input[train], output[train], epochs=30, batch_size=50, callbacks=[es])
+# evaluate the model
+scores = model.evaluate(input[test], output[test], verbose =0)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+cvscores.append(scores[1] * 100)
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+#_, acc = model.evaluate(valid_x, valid_y)
+#print('Accuracy: {0}'.format(acc * 100))
+
